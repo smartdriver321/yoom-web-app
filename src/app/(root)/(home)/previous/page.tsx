b5faa42b43c1985fde07e04b3344125b33b44edf
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-26 17:32:57 +0700

    Create Previous meeting page

--- src/app/(root)/(home)/previous/page.tsx
+++ src/app/(root)/(home)/previous/page.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import CallList from '@/components/shared/CallList'
 
-export default function Previous() {
-	return <div>Previous</div>
+export default function PreviousPage() {
+	return (
+		<section className='flex size-full flex-col gap-10 text-white'>
+			<h1 className='text-3xl font-bold'>Previous Calls</h1>
+
+			<CallList type='ended' />
+		</section>
+	)
 }
