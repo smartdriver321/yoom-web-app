e9022c10eb94da5422098163a140d24a1adf1ae1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-26 11:33:10 +0700

    Configure Clerk authentication

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
 	return (
@@ -19,6 +20,10 @@ const Navbar = () => {
 				</p>
 			</Link>
 			<div className='flex-between gap-5'>
+				<SignedIn>
+					<UserButton />
+				</SignedIn>
+
 				<MobileNav />
 			</div>
 		</nav>
