b066786c021b23e907acb707a2855f0abd418384
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-26 10:42:14 +0700

    Add Sidebar to HomeLayout

--- src/app/(root)/(home)/layout.tsx
+++ src/app/(root)/(home)/layout.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next'
 import { ReactNode } from 'react'
 
+import Sidebar from '@/components/layout/Sidebar'
+
 export const metadata: Metadata = {
 	title: 'YOOM',
 	description: 'A workspace for your team, powered by Stream Chat and Clerk.',
@@ -13,7 +15,7 @@ export default function HomeLayout({
 		<main className='relative'>
 			Navbar
 			<div className='flex'>
-				Sidebar
+				<Sidebar />
 				<section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
 					<div className='w-full'>{children}</div>
 				</section>
